245dac3a68a5ae70e9cac0c52b10eec5f9384182
prowebdev110 <tieosong119@gmail.com>
2022-11-29 00:26:55 +0900

    Update FirstSection.tsx

--- src/Components/FirstSection.tsx
+++ src/Components/FirstSection.tsx
@@ -21,7 +21,6 @@ const FirstSection = () => {
 		setTimeout(() => {
 			setTimeout(() => {
 				setTimeout(() => {
-					setImageClass('fade-in');
 				}, 100);
 				if (imageNumber >= IMG_URLS.length - 1) {
 					setImageNumber(0);
