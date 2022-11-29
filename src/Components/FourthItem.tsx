87555c9d3b01efb457e03370d430fe133d43751c
prowebdev110 <tieosong119@gmail.com>
2022-11-30 01:00:15 +0900

    Update FourthItem.tsx

--- src/Components/FourthItem.tsx
+++ src/Components/FourthItem.tsx
@@ -9,10 +9,10 @@ const FourthItem: React.FC<{
 		<div className="relative shadow-lg  h-[210px]">
 			<img
 				src={`/png/${backgroundImage}.png`}
-				className="absolute -z-10 w-full  h-full rounded-xl  "
+				className="absolute -z-10 w-full  h-full rounded-xl"
 				alt=""
 			/>
-			<div className="w-full h-full flex flex-col items-center justify-center space-y-6 text-white font-semibold text-xl  absolute z-10 top-0">
+			<div className="w-full h-full flex flex-col items-center justify-center space-y-6 text-white font-semibold text-xl absolute z-10 top-0">
 				{/* ICON */}
 				<div>
 					<img src={`/svg/${contentImage}.svg`} alt="" />
