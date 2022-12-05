1d2eafe56cbfaddd144dd573accb6930b75b0149
prowebdev110 <tieosong119@gmail.com>
2022-12-05 15:46:19 +0900

    Update Footer.tsx

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -3,10 +3,6 @@ const Footer = () => {
 		<div className="bg-[#191B1E] py-5 text-xs  pl-5 lg:pl-10  text-white">
 			<div className="flex relative ">
 				<div className=" lg:tracking-wider w-[70%] ">
-					Superintendencia de Servicios de Salud | Órgano Control de Obras Sociales |
-					0800-222-SALUD (72583) | <span className="underline">www.sssalud.gob.ar</span> |{' '}
-					<span className="underline">Dar de baja el servicio</span> |
-					<span className="underline"> Formulario de arrepentimiento</span>
 				</div>
 
 				<button className="bg-gray-100  p-3 lg:p-5 absolute right-4 lg:right-10 bottom-0 rounded-[50%] shadow-xl">
