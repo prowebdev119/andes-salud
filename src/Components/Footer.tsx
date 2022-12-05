0b6c2ffea3560a9fa12236775e52a747e3448cf9
prowebdev110 <tieosong119@gmail.com>
2022-12-05 15:46:39 +0900

    Update Footer.tsx

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -3,6 +3,7 @@ const Footer = () => {
 		<div className="bg-[#191B1E] py-5 text-xs  pl-5 lg:pl-10  text-white">
 			<div className="flex relative ">
 				<div className=" lg:tracking-wider w-[70%] ">
+					0800-222-SALUD (72583) | <span className="underline">www.sssalud.gob.ar</span> |{' '}
 					<span className="underline">Dar de baja el servicio</span> |
 					<span className="underline"> Formulario de arrepentimiento</span>
 				</div>
