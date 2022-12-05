c907a0bdae058690308ca0383280e1ee3fba9e68
prowebdev110 <tieosong119@gmail.com>
2022-12-05 15:46:27 +0900

    Update Footer.tsx

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -3,6 +3,7 @@ const Footer = () => {
 		<div className="bg-[#191B1E] py-5 text-xs  pl-5 lg:pl-10  text-white">
 			<div className="flex relative ">
 				<div className=" lg:tracking-wider w-[70%] ">
+					<span className="underline"> Formulario de arrepentimiento</span>
 				</div>
 
 				<button className="bg-gray-100  p-3 lg:p-5 absolute right-4 lg:right-10 bottom-0 rounded-[50%] shadow-xl">
