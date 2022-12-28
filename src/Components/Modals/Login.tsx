bc1812ec6f73a046a389e57abe5b9e0d082c2095
prowebdev110 <tieosong119@gmail.com>
2022-12-28 18:02:15 +0900

    Update Login.tsx

--- src/Components/Modals/Login.tsx
+++ src/Components/Modals/Login.tsx
@@ -21,6 +21,7 @@ const Login: React.FC<{ setOpen: Function; setRegisterOpen: Function }> = ({
 					<div className="login-form">
 						<label htmlFor="username">Contraseña</label>
 						<div className="relative">
+								type={passwordVisible ? 'text' : 'password'}
 								id="username"
 								className="!pr-[45px]"
 								placeholder="Ingresá tu contraseña"
